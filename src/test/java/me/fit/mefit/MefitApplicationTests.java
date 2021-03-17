a0f7f45778d70fb42d2f60f9cbd73afc0eff3795
geodk00 <tor@leeberg.dk>
2021-03-17 11:17:35 +0100

    Chore: Spring App skeleton

---
+++ src/test/java/me/fit/mefit/MefitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fit.mefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
