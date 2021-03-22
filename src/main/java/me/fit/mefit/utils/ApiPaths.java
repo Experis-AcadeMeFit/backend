fccd379dd4a327d7bcfb1566db0c5f7867a8aac0
geodk00 <tor@leeberg.dk>
2021-03-22 14:40:07 +0100

    Chore: Rename API paths to plural nouns

--- src/main/java/me/fit/mefit/utils/ApiPaths.java
+++ src/main/java/me/fit/mefit/utils/ApiPaths.java
@@ -5,10 +5,10 @@ public class ApiPaths {
     public static final String BASE_PATH = "/api/v" + VERSION;
     public static final String LOGIN_PATH = BASE_PATH + "/login";
     public static final String EXERCISE_PATH = BASE_PATH + "/exercises";
-    public static final String WORKOUT_PATH = BASE_PATH + "/workout";
-    public static final String GOAL_PATH = BASE_PATH + "/goal";
-    public static final String PROFILE_PATH = BASE_PATH + "/profile";
-    public static final String USER_PATH = BASE_PATH + "/user";
+    public static final String WORKOUT_PATH = BASE_PATH + "/workouts";
+    public static final String GOAL_PATH = BASE_PATH + "/goals";
+    public static final String PROFILE_PATH = BASE_PATH + "/profiles";
+    public static final String USER_PATH = BASE_PATH + "/users";
     public static final String PUBLIC_PATH = LOGIN_PATH + "/**";
     public static final String PROTECTED_PATH = BASE_PATH + "/**";
 }
